ee0965840f77b11863bacdf49c07c842e34f780e
Jett Jenga God <jetlewis@ucdavis.edu>
2019-12-25 21:40:36 -0800

    Actual factual final

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    development: {
+        host: '0.0.0.0',
+        dialect: 'sqlite',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        // Security note: the database is saved to the file `database.sqlite` on the local filesystem. It's deliberately placed in the `.data` directory
+        // which doesn't get copied if someone remixes the project.
+        storage: '.data/' + process.env.NODE_ENV + 'EloDB.sqlite'
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        dialect: "sqlite",
+        storage: ":memory:"
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOSTNAME,
+        dialect: 'mysql',
+        use_env_variable: 'DATABASE_URL'
+    }
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,34 +1,19 @@
+'use strict';
+
 import fs from 'fs';
 import path from 'path'
 import Sequelize from "sequelize";
-let dbName, dbUser, dbPass, options, sequelize;
-dbUser = process.env.DB_USER;
-dbPass = process.env.DB_PASS;
-const production = {
-    url: process.env.DATABASE_URL,
-        dialect: 'postgres',
-};
+let sequelize;
 
-dbName = 'EloDB';
-options = {
-    host: '0.0.0.0',
-    dialect: 'sqlite',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    },
-    // Security note: the database is saved to the file `database.sqlite` on the local filesystem. It's deliberately placed in the `.data` directory
-    // which doesn't get copied if someone remixes the project.
-    storage: '.data/' + process.env.NODE_ENV + 'EloDB.sqlite'
-};
+let basename  = path.basename(__filename);
+let env       = process.env.NODE_ENV || 'development';
+let config    = require('../config/config.js')[env];
+let db        = {};
 
-const basename = path.basename(__filename);
-const db = {};
-if (process.env.DB_URL) {
-    sequelize = new Sequelize(process.env.DB_URL, production);
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(dbName, dbUser, dbPass, options);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -37,7 +22,7 @@ fs
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
-        const model = sequelize['import'](path.join(__dirname, file));
+        let model = sequelize['import'](path.join(__dirname, file));
         db[model.name] = model;
     });
 
